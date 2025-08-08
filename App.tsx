2a3ca5089610c5870b023abdcef7dd08a28f8e7f
Pablo Barrios <exepablo96@gmail.com>
2025-08-08 16:45:42 -0300

    add button and function to set state

--- App.tsx
+++ App.tsx
@@ -1,11 +1,20 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { useState } from "react";
+import { Pressable, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
+  const [count, setCount] = useState(10);
   return (
     <View style={styles.container}>
-      <Text>Hello world</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.textHuge}>{count}</Text>
+      <Pressable
+        style={styles.floatingButton}
+        onPress={() => setCount(count + 1)}
+        onLongPress={() => setCount(0)}
+      >
+        <Text style={{ color: "white", fontSize: 20 }}>+1</Text>
+      </Pressable>
+      <StatusBar style="dark" />
     </View>
   );
 }
@@ -16,5 +25,19 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+  textHuge: {
+    fontSize: 120,
+    fontWeight: "100",
+  },
+  floatingButton: {
+    position: "absolute",
+    bottom: 20,
+    right: 20,
+    backgroundColor: "#65558F",
+    padding: 20,
+    borderRadius: 15,
+    shadowOpacity: 0.3,
+    elevation: 3,
   },
 });
